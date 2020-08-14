b24eb8e816fc7651ab67aa1f633aac509814c978
Rebecca Hawkenson <rehawkenson@gmail.com>
2020-08-14 12:55:16 -0500

    Identify the subproblems that need to be solved

---
+++ substrings.rb
@@ -0,0 +1,9 @@
+#Subproblems to solve:
+  #create a dictionary
+  #get a word from the user 
+  #run the word through the dictionary; check if the dictionary contains that word
+  #return a hash with all instances as keys
+  #include in hash a count of how many times the instance occured as values
+
+
+
